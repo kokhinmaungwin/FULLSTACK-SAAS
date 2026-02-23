413db7b28e3fdf25e5183a9f311981e13118bb9b
kokhinmaungwin <khinmaungwin675@gmail.com>
2026-02-23 22:51:18 +0630

    Add database connection setup using mongoose

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+module.exports = ()=>{
+  mongoose.connect(process.env.MONGO_URI)
+  .then(()=> console.log("DB connected"))
+  .catch(err=> console.log(err));
+};
